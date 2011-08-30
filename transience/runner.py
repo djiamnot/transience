9523940359ba4f111f2bef320a637637d9c6a6b3
Michal Seta <djiamnot@gmail.com>
2011-08-29 22:35:06 -0400

    added runner

---
+++ transience/runner.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Pyospat
+# Copyright (C) 2011 Alexandre Quessy
+# Copyright (C) 2011 Michal Seta
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pyospat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pyospat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The Runner function.
+"""
+from optparse import OptionParser
+from transience import __version__
+#from pyospat import server
+#from pyospat import application
+from pyospat import configuration
+import os
+import sys
+import time
+
+DESCRIPTION = "Python audio renderer for SpatOSC"
+
+def run():
+    """
+    Runs the application.
+    """
+    parser = OptionParser(usage="%prog [options]", version="%prog " + __version__, description=DESCRIPTION)
+    parser.add_option("-v", "--verbose", action="store_true", help="Makes the output verbose.")
+    parser.add_option("-p", "--osc-send-port", type="int", default=7000, help="UDP port to send OSC to INScore.  Default is 7000")
+    (options, args) = parser.parse_args()
+    config = configuration.Configuration()
+    if options.verbose:
+        config.verbose = True
+    if options.osc_receive_port:
+        config.osc_receive_port = options.osc_receive_port
+
+    s = server.ServerWrapper(use_twisted=True)
+    app = application.Application(config)
+    s.run()
+    # why does it often end with a segmentation fault?
+
