997e3902e99db9f434d1db5e42915399bc0f0e97
Michal Seta <djiamnot@gmail.com>
2011-08-31 00:58:58 -0400

    simple configuration - default.  needs to be expanded

---
+++ transience/configuration.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Transience
+# Copyright (C) 2011 Alexandre Quessy
+# Copyright (C) 2011 Michal Seta
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Transience is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Transience.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The Configuration class.
+"""
+
+class Configuration(object):
+    """
+    Configuration for the application.
+    """
+    def __init__(self):
+        self.verbose = False
+        self.osc_send_port = 7000
+
