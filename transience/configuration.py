b37d43c56c9303c51d708dd96b68833466cba841
Michal Seta <mis@artengine.ca>
2011-09-07 08:49:32 -0400

    Refactoring:  moved PrefParser class from application.py to configuration.py

--- transience/configuration.py
+++ transience/configuration.py
@@ -22,6 +22,57 @@
 The Configuration class.
 """
 
+from ConfigParser import SafeConfigParser
+import random
+
+class PrefParser(object):
+    """
+    Parses ~/.transiencerc and extracts the elements needed for display(s)
+    """
+    def __init__(self, file_name=None):
+        """
+        @throw IOError if file not found
+        """
+        if file_name is None:
+            file_name = os.path.expanduser("~/.transiencerc")
+        self._file_name = file_name
+        self._parse()
+
+    def _parse(self):
+        if not os.path.exists(self._file_name):
+            self._default()
+        parser = SafeConfigParser()
+        parser.read(self._file_name)
+        for section_name in parser.sections():
+            print("Section: {}".format(section_name))
+            print("   Options: {}".format(parser.options(section_name)))
+            for name, value in parser.items(section_name):
+                print ("      {} : {}".format(name, value))
+
+    def _default(self):
+        """
+        write a default configuration file
+        """
+        # TODO: replace the random for something more meaningful
+        config_file = os.path.expanduser("~/.transiencerc")
+        parser = SafeConfigParser()
+        for i in range (9):
+            page = "page {}".format(i)
+            parser.add_section(page)
+            parser.set(page, "recitation", "{}".format(random.randint(0,9)))
+            parser.set(page, "mood", "{}".format(random.randint(0,9)))
+            parser.set(page, "instructions", "{}".format(random.randint(0,9)))
+            parser.set(page, "durations", "{}".format(random.randint(0,9)))
+            parser.set(page, "glissandis", "{}".format(random.randint(0,9)))
+            parser.set(page, "interactions", "{}".format(random.randint(0,9)))
+            parser.set(page, "envelopes", "{}".format(random.randint(0,9)))
+            parser.set(page, "melos", "{}".format(random.randint(0,9)))
+            parser.set(page, "rhythms", "{}".format(random.randint(0,9)))
+            parser.set(page, "poems", "{}".format(random.randint(0,9)))
+
+        with open(config_file,"w") as f:
+            parser.write(f)
+
 class Configuration(object):
     """
     Configuration for the application.
