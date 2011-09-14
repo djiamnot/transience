a28804236639d4fde16cbcb5137bfa2f4e731fd5
Michal Seta <mis@artengine.ca>
2011-09-13 20:46:28 -0400

    score classes but will not use

--- transience/score.py
+++ transience/score.py
@@ -17,9 +17,47 @@
 # You should have received a copy of the GNU General Public License
 # along with Transience.  If not, see <http://www.gnu.org/licenses/>.
 """
-Transience score logic
+Transience score 
 """
 import inscore
+
+class ScoreElement(object):
+    """
+    Main container for a INSCore element.  This class (and its derived classes)
+    is meant to provide the proper OSC messages to be used by INScore engine.
+    """
+    def __init__(self, URI, x=0.0, y=0.0):
+        self.show = 1
+        self.x = x
+        self.y = y
+        self.scale = 1.0
+        self.URI = URI
+
+    def del(self):
+        """
+        Delete an element
+        """
+        return [self.URI, "del"]
+
+    def x(self, xpos):
+        self.x = xpos
+        return [self.URI, "x", xpos]
+
+    def y(self, ypos):
+        self.y = ypos
+        return [self.URI, "y", ypos]
+
+    def scale(self, factor):
+        self.scale = factor
+        return [self.URI, "scale", factor]
+
+class ImageElement(ScoreElement):
+    """
+    Container for images in INSCore
+    """
+    def __init__(self, URI, x=0.0, y=0.0, imgPath):
+        self.imgPath = imgPath
+        
 
 # TODO: write the score
 
