8aba4f861a3ff032a9da51242151a91602868eb3
Michal Seta <mis@artengine.ca>
2011-10-16 00:26:55 -0400

    Another, different test of setattr.

--- prototypes/dynamic_attributes/dynamic_attributes.py
+++ prototypes/dynamic_attributes/dynamic_attributes.py
@@ -13,6 +13,8 @@ class C():
 
     def set_remote(self, name, val):
         setattr(name, 'a', val)
+        setattr(name, 'b', val*2)
+        setattr(name, 'z', val*val)
 
 
 aa = A()
@@ -22,3 +24,18 @@ print("this is the c instance of A: {}".format(cc))
 print("The original .a was changed by C so is: {}".format(aa.a))
 print("The original cc.c.a should now be 99 but is: {}".format(cc.c.a))
 
+
+class Z():
+    pass 
+
+r = Z()
+
+mylist = [1,2,3,4,5]
+for i in mylist:
+    setattr(r, 'var'+str(i), i)
+
+print(r.var1)
+print(r.var2)
+print(r.var3)
+print(r.var4)
+print(r.var5)
