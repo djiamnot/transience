606c8cf7bdc3dac5c9917c67bf283c2b1732a886
Michal Seta <mis@artengine.ca>
2011-10-17 01:12:24 -0400

    UI looks ok now

--- transience/conf_ui.py
+++ transience/conf_ui.py
@@ -60,6 +60,9 @@ class ConfScreen(object):
         if message.getValues()[0] == 'conf/' and message.getValues()[1] == "clicked":
             print("Something on conf/ was clicked")
             
+    def hover_handler(self):
+        pass
+
     def _init_conf_screen(self):
         self.sconf._send(osc.Message("/ITL/conf","new"))
         self.sconf._send(osc.Message("/ITL/conf","width", 2.39062))
@@ -68,7 +71,7 @@ class ConfScreen(object):
                                      "Transience Configuration"))
         self.sconf._send(osc.Message("/ITL/conf/title", "scale", 4.0))
         self.sconf._send(osc.Message("/ITL/conf/title", "x", 0.0))
-        self.sconf._send(osc.Message("/ITL/conf/title", "y", -0.7))
+        self.sconf._send(osc.Message("/ITL/conf/title", "y", -0.9))
         self.make_save_button()
         self.make_cancel_button()
         self.set_conf_page()
@@ -111,32 +114,65 @@ class ConfScreen(object):
                                      "127.0.0.1:7001/mouse", "save_conf", "clicked"))
 
     def set_conf_page(self):
-        sequence = [1,2,3,4, 5]
-        self.recitations = ConfStrip()
-        _x = -0.9
-        _y = -0.5
+        #sequence = [1,2,3,4, 5]
+        #self.recitations = ConfStrip()
+        _y = -0.7
         _component = "conf/"
-        for i in sequence:
-            setattr(self.recitations, "recitations%s"%(str(i)), score.Element(
-                x = _x,
-                y = _y,
-                URI = "recitations"+str(i),
-                path = "recitations",
-                number = i,
-                scale = 0.7
-                ))
-            exec("self.recitations.recitations%s.component = _component"%(str(i)))
-            self.sconf._send(eval("self.recitations.{}.image()".format("recitations"+str(i))))
-            exec("self.recitations.recitations%s.stack_sequence = self.arrangement['recitations']"%(str(i)))
-            eval("self.sconf._send(self.recitations.recitations%s.get_x())"
-                 %(str(i)))
-            eval("self.sconf._send(self.recitations.recitations%s.get_y())"
-                 %(str(i)))
-            eval("self.sconf._send(self.recitations.recitations%s.scale_element())"
-                 %(str(i)))
-            eval("self.sconf._send(self.recitations.recitations%s.watch_mouse_down())"
-                 %(str(i)))
-            _x += 0.4
+        for element in self.arrangement:
+            if element == 'poems' or element == 'jtexts':
+                pass
+            else:
+                _x = -0.9
+                print("Creating", "self.{} = ConfStrip()".format(element))
+                exec("self.{} = ConfStrip()".format(element))
+                for stack in self.arrangement[element]:
+                    setattr(eval("self.{}".format(element)), "{}{}"
+                            .format(element,str(stack)), score.Element(
+                        x = _x,
+                        y = _y,
+                        URI = element+str(stack),
+                        path = element,
+                        number = stack,
+                        scale = self.scale_by_element(element)
+                        ))
+                    exec("self.{0}.{0}{1}.component = _component"
+                         .format(element, str(stack)))
+                    eval("self.sconf._send(self.{0}.{0}{1}.image())"
+                         .format(element, str(stack)))
+                    exec("self.{0}.{0}{1}.stack_sequence = self.arrangement['{0}']"
+                         .format(element, str(stack)))
+                    eval("self.sconf._send(self.{0}.{0}{1}.get_x())"
+                         .format(element, str(stack)))
+                    eval("self.sconf._send(self.{0}.{0}{1}.get_y())"
+                         .format(element, str(stack)))
+                    eval("self.sconf._send(self.{0}.{0}{1}.scale_element())"
+                         .format(element, str(stack)))
+                    eval("self.sconf._send(self.{0}.{0}{1}.watch_mouse_down())"
+                         .format(element, str(stack)))
+                    _x += 0.4
+                _y += 0.15
+            
+    def scale_by_element(self, element):
+        if element == 'rhythms':
+            return 0.25
+        elif element == 'melos':
+            return 0.3
+        elif element == 'interactions':
+            return 0.2
+        elif element == 'durations':
+            return 0.5
+        elif element == 'instructions':
+            return 0.5
+        elif element == 'glissandis':
+            return 0.1
+        elif element == 'moods':
+            return 0.5
+        elif element == 'envelopes':
+            return 0.3
+        elif element == 'etexts':
+            return 0.3
+        elif element == 'recitations':
+            return 0.5
 
 class ConfStrip(object):
     """
